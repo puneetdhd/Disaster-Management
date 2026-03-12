320eb4c766aaa14ab73de67061bc3f32a8e6c14a
Puneet <157778574+puneetdhd@users.noreply.github.com>
2026-03-12 09:01:15 +0530

    py deploy

--- importer.py
+++ importer.py
@@ -110,4 +110,5 @@ def update_map(n):
     return {'data': [trace], 'layout': layout}
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=False)
